1c27f4f35ea3af0c394cc344c4b90286f32ed43e
Bamiyor <Bamiyor>
2023-11-28 12:28:56 -0800

    100 print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-i = 0
-for x in range(ord('z'), ord('a')-1, -1):
-    print("{}".format(chr(x-i) if i == 32 else i-32), end="")
+for i in range(ord('z'), ord('a') -1, -2):
+    print("{}{}".format(chr(i), chr(i - 33)), end='')
