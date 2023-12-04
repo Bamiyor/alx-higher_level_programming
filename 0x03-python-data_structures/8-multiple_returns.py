176354d1695c9c0a7d377668b9fb348af67af5c1
Bamiyor <Bamiyor>
2023-12-03 23:37:43 -0800

    data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-add_tuple = __import__('7-add_tuple').add_tuple
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+def multiple returns(sentence):
+    my_tuple = ()
+    if len(sentence) == 0:
+        my_tuple = 0, "None"
+    else:
+        my_tuple = len(sentence), sentence[0]
+    return my_tuple
